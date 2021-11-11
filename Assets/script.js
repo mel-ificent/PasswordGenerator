832bcd3e70d89d0b6271555953da5d5250076c58
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2021-11-11 10:05:46 -0500

    added logic for checking and adding password length critera

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Password prompts
+
+function generatePassword(){
+    var passwordCriteria = [];
+    var passwordLength = parseInt(prompt("How many characters long do you want your password to be?"));
+
+//If statement to check whether number is at least 8 characters and no more than 128 characters and is an actual number
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
+        alert("Please enter a number greater than 8 and less than 128");
+        generatePassword();
+    }
+    
+    else {
+        passwordCriteria[0] = passwordLength;
+
+        var passwordLength = prompt("Would you like to include lowercase characters? (Please type 'yes' or 'no')");
+    }
+}
+
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 

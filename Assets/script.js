d9debfd2151f38f8d9aba5679338b464193f76da
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2021-11-13 12:44:50 -0500

    Updated README, added additional checks on user entries

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,7 @@ function generatePassword(){
 //If statement to check whether number is at least 8 characters and no more than 128 characters and is an actual number
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128){
         alert("Please enter a number greater than 8 and less than 128");
-        generatePassword();
+        return '';
     }
 
 //Additional prompts to ask user what other criteria needs to be met for the password.  Update passwordCriteria array with user's responses
@@ -31,10 +31,16 @@ function generatePassword(){
 
         var passwordUpperCase = confirm("Click OK to confirm including uppercase characters.");
         passwordCriteria[4] = passwordUpperCase;
+
+//Added a check to confirm that the end user entered at least one password criteria, otherwise they are prompted to try again
+        if(!passwordCriteria[1] &&!passwordCriteria[2] && !passwordCriteria[3] && !passwordCriteria[4]){
+            alert("You did not choose any password criteria.  Please try again.");
+            return '';
+        }
     }
 
     //For loop to generate a password character for the length of the password defined by the user
-    //In the loop we have a series of if statements to determine the criteria combination given by the user
+    //In the loop we have a series of if statements to determine a valid set of values that can be used for each character of the password based on the user's criteria
     for(i=0; i<passwordCriteria[0]; i++){
     if(passwordCriteria[1]){
             
@@ -56,10 +62,15 @@ function generatePassword(){
         characterOption.push(character.charAt(Math.floor(Math.random() * character.length)).toUpperCase());
 
      }
- 
-     var x = Math.floor((Math.random() * characterOption.length));
-     passwordArray.push(characterOption[x]);
+    //Once we have the list of possible password character values, we are going to pick one of the values at random and add it to our password array
+    //However, we need to make sure that at least one character of the password meets each user requirement
+     
+
+    var x = Math.floor((Math.random() * characterOption.length));
+    passwordArray.push(characterOption[x]);
+
    }
+  //after defining the passwordArray return a string value of the Array without commas 
    return passwordArray.join("");
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PasswordGenerator
+
+The purpose of this project is to create a password generator application which takes in criteria from a user (i.e. password length, types of characters to be used) and generates a password.
+
+This 
+
+## Image of the Password Generator
+![Password Generator](./images/passwordGenerator.png)
